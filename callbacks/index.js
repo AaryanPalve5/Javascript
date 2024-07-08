2652703ec50267100b2be466258770f1c23e99db
AaryanPalve5 <aspalve2004@gmail.com>
2024-07-08 16:59:15 +0530

    Callbacks

--- callbacks/index.js
+++ callbacks/index.js
@@ -1,5 +1,5 @@
 //callback= a function that is passed as an argument to another function
-
+/*
 
 hello(leave);
 function hello(callback){
@@ -15,4 +15,29 @@ function goodbye(){
 
 function leave(){
     console.log("Leave!");
+}
+goodbye()
+*/
+
+// Sum
+// Example with Sum and Callbacks
+
+// Calling sum with different callbacks
+sum(displayConsole, 4, 5); // Displays result in console
+sum(displayPage, 10, 20); // Displays result on a web page
+
+// sum function definition
+function sum(callback, x, y) {
+    let result = x + y;
+    callback(result); // Execute the callback function with the result
+}
+
+// Callback function to display result in console
+function displayConsole(result) {
+    console.log("Result (Console):", result);
+}
+
+// Callback function to display result on a webpage
+function displayPage(result) {
+    document.getElementById("myH1").textContent = "Result (Page): " + result;
 }
